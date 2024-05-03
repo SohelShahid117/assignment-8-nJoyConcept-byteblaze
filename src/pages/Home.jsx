b6a8ac0d5af6d5294f06077e4f6e374a67e1443a
SohelShahid117 <sohelshahid09@gmail.com>
2024-05-04 00:35:54 +0600

    start from part-3 30 min

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@ import Navbar from "../Components/Navbar/Navbar";
 import Hero from "./../Components/Hero/Hero";
 //https://github.com/shakilahmedatik/byteblaze--------atikVai-ByteBlaaze
 //https://loading.io/background/-----------------animation bg/pic/loading
+//https://www.andrealves.dev/blog/how-to-make-an-animated-gradient-text-with-tailwindcss/-----animated text
+//https://developers.forem.com/api/v1
+//https://mambaui.com/components/blog------------vvi
 
 const Home = () => {
   return (
