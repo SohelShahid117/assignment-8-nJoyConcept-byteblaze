2c1cc0a7b1c2cbcebb0cba56d88680d9fcd0c670
SohelShahid117 <sohelshahid09@gmail.com>
2024-05-04 11:47:23 +0600

    part-3 done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,10 @@ import Hero from "./../Components/Hero/Hero";
 //https://www.andrealves.dev/blog/how-to-make-an-animated-gradient-text-with-tailwindcss/-----animated text
 //https://developers.forem.com/api/v1
 //https://mambaui.com/components/blog------------vvi
+//https://developers.forem.com/api/v1----for API
+//https://dev.to/api/articles------------for API
+//https://dev.to/api/articles?per_page=5
+//https://dev.to/api/articles?per_page=20&top=7
 
 const Home = () => {
   return (
